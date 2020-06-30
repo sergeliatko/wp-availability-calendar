bc371b716e6582db16d8e5a7147e39697e94f859
sergeliatko <serge@techspokes.com>
2020-06-30 12:55:29 +0200

    Modified translator function

--- src/DateFormatTranslateTrait.php
+++ src/DateFormatTranslateTrait.php
@@ -16,18 +16,67 @@ trait DateFormatTranslateTrait {
 	 * @return string
 	 */
 	protected static function PHPDateFormatToJSDatePicker( string $format ): string {
-		return str_replace(
-			array(
-				'Y',
-				'm',
-				'd',
-			),
-			array(
-				'yy',
-				'mm',
-				'dd',
-			),
-			$format
+		$rules         = array(
+			// Day
+			'd' => 'dd',
+			'D' => 'D',
+			'j' => 'd',
+			'l' => 'DD',
+			'N' => '',
+			'S' => '',
+			'w' => '',
+			'z' => 'o',
+			// Week
+			'W' => '',
+			// Month
+			'F' => 'MM',
+			'm' => 'mm',
+			'M' => 'M',
+			'n' => 'm',
+			't' => '',
+			// Year
+			'L' => '',
+			'o' => '',
+			'Y' => 'yy',
+			'y' => 'y',
+			// Time
+			'a' => '',
+			'A' => '',
+			'B' => '',
+			'g' => '',
+			'G' => '',
+			'h' => '',
+			'H' => '',
+			'i' => '',
+			's' => '',
+			'u' => '',
 		);
+		$jquery_format = '';
+		$escaping      = false;
+		for ( $i = 0; $i < strlen( $format ); $i ++ ) {
+			$char = $format[ $i ];
+			// PHP date format escaping character
+			if ( $char === '\\' ) {
+				$i ++;
+				if ( $escaping ) {
+					$jquery_format .= $format[ $i ];
+				} else {
+					$jquery_format .= "'" . $format[ $i ];
+				}
+				$escaping = true;
+			} else {
+				if ( $escaping ) {
+					$jquery_format .= "'";
+					$escaping      = false;
+				}
+				if ( isset( $rules[ $char ] ) ) {
+					$jquery_format .= $rules[ $char ];
+				} else {
+					$jquery_format .= $char;
+				}
+			}
+		}
+
+		return $jquery_format;
 	}
 }
